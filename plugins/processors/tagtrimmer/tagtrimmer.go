bdd75caf4fa560af8718c5b4630d9ea131d136bd
Brianne Matthews <brianne_matthews@intuit.com>
2019-07-01 08:56:00 -0700

    Added tagtrimmer

---
+++ plugins/processors/tagtrimmer/tagtrimmer.go
@@ -0,0 +1,52 @@
+package tagtrimmer
+
+import (
+	"strings"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/processors"
+)
+
+const sampleConfig = `
+ # tag_name = "intuit_offeringid"
+ # trim_values = ["mint", "qbo", "qbse", "qbf", "turbotax", "account_view",
+  "triage", "fdptools", "6", "ctg"]
+`
+
+// TagTrimmer ...
+type TagTrimmer struct {
+	TagName string `toml:"tag_name"`
+	TrimValues []string `toml:"trim_values"`
+}
+
+// SampleConfig ...
+func (tt *TagTrimmer) SampleConfig() string {
+	return sampleConfig
+}
+
+// Description ...
+func (tt *TagTrimmer) Description() string {
+	return "TagTrimmer trims tag values containing a given string down to that string."
+}
+
+
+// Apply ...
+func (tt *TagTrimmer) Apply(in ...telegraf.Metric) []telegraf.Metric {
+	for _, metric := range in {
+		if val, ok := metric.GetTag(tt.TagName); ok {
+			for _, trimValue := range tt.TrimValues {
+				if strings.Contains(val, trimValue) {
+					metric.RemoveTag(tt.TagName)
+					metric.AddTag(tt.TagName, trimValue)
+					break
+				}
+			}
+		}
+	}
+	return in
+}
+
+func init() {
+	processors.Add("tagtrimmer", func() telegraf.Processor {
+		return &TagTrimmer{}
+	})
+}
