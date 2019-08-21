bb3462de8a1497bd9d6fe6a5fb4eb05eebddc91a
Brianne Matthews <brianne_matthews@intuit.com>
2019-08-21 13:41:25 -0700

    Added redismapper

---
+++ plugins/processors/redismapper/redismapper.go
@@ -0,0 +1,113 @@
+package redismapper
+
+import (
+	"strings"
+
+	"github.com/go-redis/redis"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/processors"
+)
+
+type RedisMapper struct {
+	client      *redis.Client
+	Addr        string
+	Password    string
+	DB          int
+	Conversions []conversion
+}
+
+type conversion struct {
+	OldKey       string
+	NewKey       string
+	Contains     bool
+	HandleQuotes bool
+	CacheName    string
+}
+
+const sampleConfig = `
+	#		addr = "localhost:6379"
+	#		password = ""
+	#		DB = 0
+	# 	[[processors.redismapper.conversions]]
+	#			## The key of the value that determines the new key's value
+	#			old_key = "id_num"
+	#			## The key to be added
+	#			new_key = "name"
+	#			## Whether the value must exactly match a key in mapping or just contain
+	#			## 	a key in the mapping to be assigned the corresponding value
+	#			# contains = false
+	#			## If true, trimms values of escape characters and quotes and adds quotes
+	#			## to keys
+	#			# handle_quotes = false
+	#			## The redis hash key with the mapping from the old key's values to the new key's values
+	#			cache_name = "NameMap"
+	# 	## More than one conversion can be added.
+	# 	## Order of conversions matters: a later conversion can access tags added by earlier conversions
+`
+
+func NewRedisMapper() *RedisMapper {
+	rm := &RedisMapper{}
+	rm.client = redis.NewClient(&redis.Options{
+		Addr:     rm.Addr,
+		Password: rm.Password,
+		DB:       rm.DB,
+	})
+	return rm
+}
+
+func (rm *RedisMapper) SampleConfig() string {
+	return sampleConfig
+}
+
+func (rm *RedisMapper) Description() string {
+	return ""
+}
+
+func (rm *RedisMapper) Apply(in ...telegraf.Metric) []telegraf.Metric {
+	for _, metric := range in {
+		for _, tagConversion := range rm.Conversions {
+			newValue := "Unknown"
+			if oldValue, ok := metric.GetTag(tagConversion.OldKey); ok {
+				if tagConversion.HandleQuotes {
+					oldValue = addQuotes(oldValue)
+				}
+				if !tagConversion.Contains {
+					if value, error := rm.client.HGet(tagConversion.CacheName, oldValue).Result(); error == nil {
+						newValue = value
+					}
+				} else {
+					if mappings, error := rm.client.HGetAll(tagConversion.CacheName).Result(); error == nil {
+						for mapKey, mapValue := range mappings {
+							if strings.Contains(oldValue, mapKey) {
+								newValue = mapValue
+								break
+							}
+						}
+					}
+				}
+			}
+			if tagConversion.HandleQuotes {
+				newValue = removeQuotes(newValue)
+			}
+			metric.AddTag(tagConversion.NewKey, newValue)
+		}
+	}
+	return in
+}
+func removeQuotes(s string) string {
+	start := strings.Index(s, "\"") + 1
+	end := strings.LastIndex(s, "\"")
+	if start < end {
+		return s[start:end]
+	}
+	return s
+}
+func addQuotes(s string) string {
+	return "\"" + s + "\""
+}
+
+func init() {
+	processors.Add("redismapper", func() telegraf.Processor {
+		return NewRedisMapper()
+	})
+}
---
+++ plugins/processors/redismapper/redismapper_test.go
@@ -0,0 +1,191 @@
+package redismapper
+
+import (
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/metric"
+	"github.com/stretchr/testify/assert"
+)
+
+func newMetric(name string, tags map[string]string, fields map[string]interface{}) telegraf.Metric {
+	if tags == nil {
+		tags = map[string]string{}
+	}
+	if fields == nil {
+		fields = map[string]interface{}{}
+	}
+	m, _ := metric.New(name, tags, fields, time.Now())
+	return m
+}
+
+func TestHandleQuotes(t *testing.T) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	//Everything outside surrounding quotes will be ignored
+	client.HMSet("TestHandleQuotes", map[string]interface{}{"\"a\"": "\x00\x00\x06\"b\""})
+	//Keys without quotes around them will be ignored (a value of "c" maps to a key of "\"c\"", not "c")
+	//Values without quotes will be unchanged
+	client.HMSet("TestHandleQuotes2", map[string]interface{}{"\"a\"": "b", "c": "d"})
+	//Interior quotes should not be removed ("\x00\x00\x06\"\"b\"c\"d\"" maps to "b\"c\"d")
+	client.HMSet("TestHandleQuotes3", map[string]interface{}{"\"a\"": "\x00\x00\x06\"\"b\"c\"d\""})
+
+	rm := NewRedisMapper()
+	rm.client = client
+	rm.Conversions = []conversion{
+		conversion{
+			HandleQuotes: true,
+			OldKey:       "name1",
+			NewKey:       "name2",
+			CacheName:    "TestHandleQuotes",
+		},
+		conversion{
+			HandleQuotes: true,
+			OldKey:       "name3",
+			NewKey:       "name4",
+			CacheName:    "TestHandleQuotes2",
+		},
+		conversion{
+			HandleQuotes: true,
+			OldKey:       "name1",
+			NewKey:       "name5",
+			CacheName:    "TestHandleQuotes3",
+		},
+	}
+	m1 := newMetric("foo1", map[string]string{"name1": "a"}, nil)
+	m2 := newMetric("foo2", map[string]string{"name1": "a", "name3": "c"}, nil)
+	results := rm.Apply(m1, m2)
+	m12 := newMetric("foo1", map[string]string{"name1": "a", "name2": "b",
+		"name4": "Unknown", "name5": "\"b\"c\"d"}, nil)
+	m22 := newMetric("foo2", map[string]string{"name1": "a", "name3": "c",
+		"name2": "b", "name4": "Unknown", "name5": "\"b\"c\"d"}, nil)
+	assert.Equal(t, m12.Tags(), results[0].Tags())
+	assert.Equal(t, m22.Tags(), results[1].Tags())
+}
+
+func TestMultiple(t *testing.T) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	client.HMSet("TestMap1", map[string]interface{}{"a": "b", "c": "d"})
+	client.HMSet("TestMap2", map[string]interface{}{"a": "b", "e": "f"})
+	client.HMSet("TestMap3", map[string]interface{}{"1": "2", "3": "4"})
+
+	rm := NewRedisMapper()
+	rm.client = client
+	rm.Conversions = []conversion{
+		conversion{
+			OldKey:    "name1a",
+			NewKey:    "name1b",
+			CacheName: "TestMap1",
+		},
+		conversion{
+			OldKey:    "name2a",
+			NewKey:    "name2b",
+			CacheName: "TestMap2",
+		},
+		conversion{
+			OldKey:    "name3a",
+			NewKey:    "name3b",
+			CacheName: "TestMap3",
+		},
+	}
+	m1 := newMetric("foo1", map[string]string{"name1a": "a", "name2a": "a"}, nil)
+	m2 := newMetric("foo2", map[string]string{"name2a": "b"}, nil)
+	m3 := newMetric("foo3", map[string]string{"name1a": "c", "name2a": "e", "name3a": "3"}, nil)
+
+	results := rm.Apply(m1, m2, m3)
+
+	m1b := newMetric("foo1", map[string]string{"name1a": "a", "name2a": "a",
+		"name1b": "b", "name2b": "b", "name3b": "Unknown"}, nil)
+	m2b := newMetric("foo2", map[string]string{"name2a": "b", "name1b": "Unknown",
+		"name2b": "Unknown", "name3b": "Unknown"}, nil)
+	m3b := newMetric("foo3", map[string]string{"name1a": "c", "name2a": "e",
+		"name3a": "3", "name1b": "d", "name2b": "f", "name3b": "4"}, nil)
+
+	assert.Equal(t, m1b.Tags(), results[0].Tags())
+	assert.Equal(t, m2b.Tags(), results[1].Tags())
+	assert.Equal(t, m3b.Tags(), results[2].Tags())
+}
+
+func TestInexact(t *testing.T) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	client.HMSet("TestMap4", map[string]interface{}{"a": "b"})
+
+	rm := NewRedisMapper()
+	rm.client = client
+	rm.Conversions = []conversion{
+		conversion{
+			OldKey:    "name1",
+			NewKey:    "name2",
+			CacheName: "TestMap4",
+			Contains:  true,
+		},
+	}
+	m1 := newMetric("foo1", map[string]string{"name1": "abc"}, nil)
+	m2 := newMetric("foo2", map[string]string{"name1": "a"}, nil)
+	m3 := newMetric("foo3", map[string]string{"name1": "bc"}, nil)
+
+	results := rm.Apply(m1, m2, m3)
+
+	m12 := newMetric("foo1", map[string]string{"name1": "abc", "name2": "b"}, nil)
+	m22 := newMetric("foo2", map[string]string{"name1": "a", "name2": "b"}, nil)
+	m32 := newMetric("foo3", map[string]string{"name1": "bc", "name2": "Unknown"}, nil)
+
+	assert.Equal(t, m12.Tags(), results[0].Tags())
+	assert.Equal(t, m22.Tags(), results[1].Tags())
+	assert.Equal(t, m32.Tags(), results[2].Tags())
+}
+
+func TestOrder(t *testing.T) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	client.HMSet("TestOrderMap1", map[string]interface{}{"1": "a"})
+	client.HMSet("TestOrderMap2", map[string]interface{}{"a": "b"})
+	client.HMSet("TestOrderMap3", map[string]interface{}{"b": "c"})
+
+	rm := NewRedisMapper()
+	rm.client = client
+	rm.Conversions = []conversion{
+		conversion{
+			OldKey:    "1",
+			NewKey:    "2",
+			CacheName: "TestOrderMap1",
+			Contains:  true,
+		},
+		//This conversion will always give unknown for m1 because the key "3" is
+		// added *after* this conversion happens
+		conversion{
+			OldKey:    "3",
+			NewKey:    "4",
+			CacheName: "TestOrderMap3",
+			Contains:  true,
+		},
+		conversion{
+			OldKey:    "2",
+			NewKey:    "3",
+			CacheName: "TestOrderMap2",
+			Contains:  true,
+		},
+	}
+	m1 := newMetric("foo1", map[string]string{"1": "1"}, nil)
+
+	results := rm.Apply(m1)
+	m12 := newMetric("foo1", map[string]string{"1": "1", "2": "a", "3": "b", "4": "Unknown"}, nil)
+
+	assert.Equal(t, m12.Tags(), results[0].Tags())
+}
